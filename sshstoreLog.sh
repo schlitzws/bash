f99d5e2c69fc7ed31bca13b8bd415a3e062dbf08
schlitzws <30422028+schlitzws@users.noreply.github.com>
2018-01-13 01:54:36 +0000

    Store ssh logs of specified target to a file.

--- sshstoreLog.sh
+++ sshstoreLog.sh
@@ -2,14 +2,7 @@
 echo "This program is used to gather your /var/log/auth.log data of a from a specified target trying to connect via ssh."
 echo "Where do you want to store the file? IF the file does not exist the file will be created for you. "
 read storel
-touch $storel
-#if [!$l]
-#then
-#touch $l
-#else
-#mv  /home/llun/Ghast/logofEst /home/llun/Ghast/PersonalLogs.txt
-#$l = /home/llun/Ghast/PersonalLogs.txt
-#fi
+touch $store1
 pwd=`pwd`
 cd $pwd
 echo "You are in $pwd"
