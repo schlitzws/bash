c8aef905a469a36e50613297f43c8f3ab395aafb
schlitzws <30422028+schlitzws@users.noreply.github.com>
2018-01-19 00:52:58 -0600

    Add files via upload

--- bttd.sh
+++ bttd.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 ipTablsrc="/home/guls/Documents/KEEP/TRGT_FLD/srcIP_final"
+echo "If you have not made your whitelist file please do so now before continuing ctrl-C to exit."
+echo "If you have the whitelist file please enter it's destination now."
+read whiteIPs
+mapfile -t warry < "$whiteIPs"
 echo "This program is used to ban any ips that have failed login in  /var/log/auth.log for ssh."
-echo "Choose the destination file for blocked ips if file doesn't exist it will be created."
+echo "Please enter the destination or name (if you want to save to the cd) of the first temp file"
 read blockIPs
 if [ ! -d $blockIPs ]
 then touch $blockIPs
 fi
-echo "Grabbing log data..."
-cat /var/log/auth.log | grep "Failed*" | cut -d ' ' -f 14 | sort -u > $blockIPs
-cat /var/log/auth.log | grep "Failed*" | cut -d ' ' -f 11 | sort -u >> $blockIPs
-cat /var/log/auth.log | grep "Failed*" | cut -d ' ' -f 13 | sort -u >> $blockIPs
-cat $blockIPs
-echo "Filtering log data..." 
-echo "Provide filtered target data destination:"
-read  filtdst
-cat $blockIPs | grep -o '[0-9,.]\+' > "$filtdst"
-cat $filtdst
+echo "Grabbing auth.log data...through select keywords."
+echo "Please enter the destination or name of the second temp file."
+read ld
+cat /var/log/auth.log | grep "Failed password"  |  sort -u >  $blockIPs
+cat /var/log/auth.log | grep "Received disconnect" | sort -u >> $blockIPs
+mapfile -t tarry < "$blockIPs"
+for ip in "${tarry[@]}"; do
+echo "$ip" | grep -o '[0-9,.]\+' | sort -u  >> $ld; done
+echo "Done Grabbing Log Data"
+#cat /var/log/auth.log | grep -o '[0-9,.]\+' | cut -d ' ' -f 25 | sort -u >> $blockIPs
+cat $ld
+echo "Please enter 3 temp file for filtering out the numbers found in the auth.log data to ips"
+read filtdst
+touch $filtdst
+cat $ld > $filtdst
 mapfile -t blkArr < "$filtdst"
 for ip in "${blkArr[@]}"; do 
 echo "$ip"; 
@@ -25,8 +34,8 @@ echo $ip >> "$ipTablsrc"
 fi ;
 done
 echo "This is the final filtered list of blocked ips."
-echo "Provide final filter location..file"
 read finFilt
+touch $finFilt
 touch $ipTablsrc
 cat $ipTablsrc | sort -u > "$finFilt"
 cat $finFilt
